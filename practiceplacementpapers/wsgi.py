5427a6aa788048e346b9e2fdae50a40e9e9de666
pycoder505 <anjaneyulu.batta505@gmail.com>
2016-03-21 22:32:46 +0530

    Update wsgi.py

--- practiceplacementpapers/wsgi.py
+++ practiceplacementpapers/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "practiceplacementpapers.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "practice-placement-papers.settings")
 
 from whitenoise.django import DjangoWhiteNoise
 
