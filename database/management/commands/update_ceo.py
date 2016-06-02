ed99e94766911a42d6af64d436c180bfeda0b6ee
Anjaneyulu <anjaneyulu.batta@micropyramid.com>
2016-06-02 11:20:01 +0530

    c language added

--- database/management/commands/update_ceo.py
+++ database/management/commands/update_ceo.py
@@ -3,6 +3,7 @@ from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 from PyQt4.QtWebKit import *
 import re
+import requests
 from database.models import Company
 
 class Render(QWebPage):
@@ -20,13 +21,18 @@ class Render(QWebPage):
 all_companies = Company.objects.all()
 for company in all_companies:
     company_name = company.name
+    print "-------input-------"
     print company.name
     print company.ceo
     print "-----------------"
-    url = 'http://www.google.com/search?hl=en-IN&source=hp&biw=&bih=&q=' + company_name + '+company+ceo'
+    # url = 'http://www.google.com/search?hl=en-IN&source=hp&biw=&bih=&q=' + company_name + '+company+ceo'
+    url = 'http://www.google.com/search?start=0&num=10&q=' + company_name + '+company+ceo+name'
+    print url
     r = Render(url)
     html = unicode(r.frame.toHtml()).encode('utf-8')
+    print html
     result = re.search('class="_m3b">(.*)</', html)
     ceo_name = result.group(1).split("</")[0]
+    print "-------output-------"
     print company.name
     print ceo_name
